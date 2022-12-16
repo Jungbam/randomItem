b5e648ef05cc23af8682a6243c91225cd482f607
Suhyeon <heypitcher@hanmail.net>
2022-12-17 01:05:35 +0900

    layout1

--- src/pages/Item/Item.js
+++ src/pages/Item/Item.js
@@ -1,7 +1,44 @@
 import React from "react";
-
+import styled from "styled-components";
+import Styled from "styled-components";
 const Item = () => {
-  return <div>Item</div>;
+  return (
+    <wrapper>
+      <search>
+        <form>
+          <input placeholder="검색 창" />
+        </form>
+      </search>
+      <nav>
+        <div>카테고리</div>
+        <div>
+          <button>ALL</button>
+          <button>분류1</button>
+          <button>분류2</button>
+          <button>분류3</button>
+          <button>분류4</button>
+
+        </div>
+      </nav>
+    </wrapper>
+  )
 };
 
 export default Item;
+
+const wrapper = styled.div`
+
+`
+const search = styled.div`
+border:5px solid #000;
+width:250px;
+height:100px;
+`
+const nav = styled.div`
+
+`
+const itemsWrapper = styled.div`
+border:5px solid #000;
+width:850px;
+height:500px;
+`
