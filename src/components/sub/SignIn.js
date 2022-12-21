283070a281b203c2486f26b27cb488d2f72796b4
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-12-21 16:12:50 +0900

    signin수정

--- src/components/sub/SignIn.js
+++ src/components/sub/SignIn.js
@@ -4,7 +4,6 @@ import { useDispatch } from "react-redux";
 import { __postSignin } from "../../redux/slice/userSlice";
 import { useNavigate } from "react-router-dom";
 import { logedIn } from "../../redux/slice/userSlice";
-
 const SignIn = ({ closeModal, showImage }) => {
   const [userEmail, setUserEmail] = useState("");
   const [userPw, setUserPw] = useState("");
@@ -15,19 +14,23 @@ const SignIn = ({ closeModal, showImage }) => {
   //     email: String,
   //       password: String,
   // }
-
   // const test = () => {
   //   dispatch(logedIn(true))
   //   console.log('야뭐해?')
 
   // }
 
-
   const submitHandler = (e) => {
     e.preventDefault();
     setUserEmail(userEmail);
     setUserPw(userPw);
-
+    dispatch(__postSignin({ email: userEmail, password: userPw }));
+    // dispatch(logedIn(true))
+    navigate("/");
+    closeModal();
+    showImage(true);
+    console.log("뭐하냐구?");
+  };
 
   return (
     <>
@@ -53,31 +56,6 @@ const SignIn = ({ closeModal, showImage }) => {
           로그인
         </StButton>
       </form>
-
-    dispatch(__postSignin({ email: userEmail, password: userPw }))
-    dispatch(logedIn(true))
-    navigate("/")
-    closeModal()
-    showImage(true)
-    console.log('뭐하냐구?')
-
-  }
-
-  return (
-    <><form onSubmit={submitHandler}>
-      <StDiv>로그인</StDiv>
-      <StWrapper>
-        <StLabel>이메일</StLabel>
-        <input value={userEmail} onChange={(e) => setUserEmail(e.target.value)} type='email'></input>
-      </StWrapper>
-      <StWrapper>
-        <StLabel>비밀번호</StLabel>
-        <input value={userPw} onChange={(e) => setUserPw(e.target.value)} type='password'></input>
-      </StWrapper>
-      <StButton type="submit">로그인</StButton>
-
-    </form>
-
     </>
   );
 };
